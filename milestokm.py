a31cdddf46f6d1b56c72f030d27261cb6f8d0e7e
Darrion Goggins <52177451+Darrion-goggins@users.noreply.github.com>
2019-12-18 14:12:30 -0600

    Made a simple miles to kilometer calculator

---
+++ milestokm.py
@@ -0,0 +1,5 @@
+print("This is used to convert miles to kilometers. Please enter the number of miles.")
+
+num = int(input("Please enter a number: "))
+kilometercalc = (num * 1.609344)
+print("The number of kilometers from what you typed is: ", round(kilometercalc, 2))
